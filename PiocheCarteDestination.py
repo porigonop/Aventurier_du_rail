8c06913753c06ab4b7ec77a84bf2bdf2e7fc74b2
Lievre <alievre@laposte.net>
2017-01-23 15:17:29 +0100

    add Pioche Carte Wagon

---
+++ PiocheCarteDestination.py
@@ -0,0 +1,11 @@
+from CarteDestination import CarteDestination
+import random
+class PiocheCarteDestination:
+    def __init__(self, lst_dest):
+        self.pioche = lst_dest
+        
+    def shuffle(self):
+        random.shuffle(self.pioche)
+    
+    def pick(self):
+        return self.pioche.pop()
