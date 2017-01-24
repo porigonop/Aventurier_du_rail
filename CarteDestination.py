36a6a670605a24f92259a4e7b728929f0f4253fc
Lrnvtk5 <lauren.vaitanaki@gmail.com>
2017-01-24 16:59:01 +0100

    add CarteDestination

--- CarteDestination.py
+++ CarteDestination.py
@@ -1,16 +1,23 @@
 #usr/bin/env python3
-
-
+####################
+#
+#
+#
+#
+####################
 class CarteDestination:
     """ """
 
-    def __init__(self, departure, arrival):
+    def __init__(self, departure, arrival, value):
         """ """
         self.departure = departure
         self.arrival = arrival
+        self.value = value
 
 
     def __repr__(self):
         """ """
         return 'Point de départ ---> ' + self.departure + ' \n'
-               ' Point d''arrivée ---> ' + self.arrival
+                + ' Point d''arrivée ---> ' + self.arrival
+    
+               
