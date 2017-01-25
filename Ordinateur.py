10e75abbfe4bb6ac8300782a0ca25fdb3c7eed72
Lrnvtk5 <lauren.vaitanaki@gmail.com>
2017-01-25 12:12:21 +0100

    Class Ordi

---
+++ Ordinateur.py
@@ -0,0 +1,8 @@
+#usr/bin/env python3
+
+class Ordinateur(Joueur):
+    """ Class which inherits from class Joueur. It allows to play against
+        a computer """
+    def jouer(self):
+        """ """
+
