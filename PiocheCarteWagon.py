85a758e0077d317d021d03757aee08da6e262aa4
Lrnvtk5 <lauren.vaitanaki@gmail.com>
2017-01-25 21:18:35 +0100

    add comment

--- PiocheCarteWagon.py
+++ PiocheCarteWagon.py
@@ -1,3 +1,9 @@
+#!usr/bin/env python3
+######################################
+#Authors: A. LIEVRE et L. VAITANAKI  #
+#Last Modification: 01/25/2017       #
+######################################
+
 from pprint import pprint
 from CarteWagon import CarteWagon
 import random
@@ -6,7 +12,12 @@ COLOR = ["Bleu", "Rose", "Orange", "Blanc",\
 
 
 class PiocheCarteWagon:
+    """ Class to model a stack of Wagon Card"""
     def __init__(self):
+        """ Class constructor to initiate the following attributes:
+        pioche: attribute for modeling a stack of wagon cards
+        defausse : attribute representing a discard
+        """
         self.pioche = [CarteWagon(COLOR[i]) \
                         for i in range(len(COLOR)-1)\
                         for j in range(12)]+\
@@ -16,15 +27,19 @@ class PiocheCarteWagon:
         
         
     def shuffle(self):
+        """ method to mix up the stack """
         random.shuffle(self.pioche)
     
     def pick(self):
+        """ method to draw a card"""
         return self.pioche.pop()
     
     def defausse(self, carte):
+        """ method to discard"""
         self.defausse.append(carte)
     
     def __repr__(self):
+        """ method that allows to print the object """
         return str(self.pioche)
     
 if __name__ == "__main__":
